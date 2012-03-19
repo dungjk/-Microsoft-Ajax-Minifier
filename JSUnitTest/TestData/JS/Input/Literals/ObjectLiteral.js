0947e3937c777038e6d529304ef45680599e9a78
ronlo_cp <SND\ronlo_cp>
2012-03-19 10:40:51 -0700

    add unit test to make sure a statement starting with an object literal gets parens put around the object literal.

--- JSUnitTest/TestData/JS/Input/Literals/ObjectLiteral.js
+++ JSUnitTest/TestData/JS/Input/Literals/ObjectLiteral.js
@@ -11,3 +11,12 @@
   };
  
 alert(o.goto);
+
+// just to make sure the expression statements before and after don't get combined
+while(0);
+
+// start a statement with an object literal needs to 
+// be wrapped in parens so it doesn't get parsed as a bad
+// statement block
+({foo: 42, showMe: function() { document.write("<h1>" + this.foo + "!</h1>") } }).showMe();
+
