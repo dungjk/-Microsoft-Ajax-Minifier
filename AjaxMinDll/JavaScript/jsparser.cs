d7e042786a690c24fbcff2d77a0c324c51bda57d
ronlo_cp <SND\ronlo_cp>
2012-12-07 12:52:31 -0800

    Fix for issue #18924 - ///#DEBUG was dependent on the StripDebugStatements kill switch, which -pretty turned off, thereby causing ///#DEBUG to be ignored. The ///#DEBUG processing needed to be wrapped under the same processing switches as other preprocessor comments, like ///#IF and ///#IFDEF.

--- AjaxMinDll/JavaScript/jsparser.cs
+++ AjaxMinDll/JavaScript/jsparser.cs
@@ -253,10 +253,6 @@ namespace Microsoft.Ajax.Utilities
             m_scanner.AllowEmbeddedAspNetBlocks = m_settings.AllowEmbeddedAspNetBlocks;
             m_scanner.IgnoreConditionalCompilation = m_settings.IgnoreConditionalCompilation;
 
-            // set the skip-debug-blocks flag on the scanner if we are stripping debug statements
-            m_scanner.SkipDebugBlocks = m_settings.StripDebugStatements
-                 && m_settings.IsModificationAllowed(TreeModifications.StripDebugStatements);
-
             // set any defines
             m_scanner.UsePreprocessorDefines = !m_settings.IgnorePreprocessorDefines;
             if (m_scanner.UsePreprocessorDefines)
